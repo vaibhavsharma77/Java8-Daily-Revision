f1431e6b47e826f9ba2ee8bef2d69cb07118e75a
Vaibhav Sharma <vaibhavsharma777@yahoo.com>
2022-02-01 21:52:05 +0100

    added initial files

---
+++ src/com/java/examples/Employee.java
@@ -0,0 +1,112 @@
+package com.java.examples;
+
+import java.util.Objects;
+
+public class Employee {
+
+    int id;
+
+    String name;
+
+    int age;
+
+    String gender;
+
+    String department;
+
+    int yearOfJoining;
+
+    double salary;
+
+    public Employee(int id, String name, int age, String gender, String department, int yearOfJoining, double salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.department = department;
+        this.yearOfJoining = yearOfJoining;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getYearOfJoining() {
+        return yearOfJoining;
+    }
+
+    public void setYearOfJoining(int yearOfJoining) {
+        this.yearOfJoining = yearOfJoining;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", department='" + department + '\'' +
+                ", yearOfJoining=" + yearOfJoining +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && age == employee.age && yearOfJoining == employee.yearOfJoining && Double.compare(employee.salary, salary) == 0 && Objects.equals(name, employee.name) && Objects.equals(gender, employee.gender) && Objects.equals(department, employee.department);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age, gender, department, yearOfJoining, salary);
+    }
+}
---
+++ src/com/java/examples/Main.java
@@ -0,0 +1,45 @@
+package com.java.examples;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Employee> employeeData = EmployeeDataFactory.getEmployeeData();
+
+        System.out.println("-------FIND THE NUMBER OF MALES AND FEMALES --------");
+        Map<String, Long> collect = employeeData.stream().collect(Collectors.groupingBy(Employee::getGender, Collectors.counting()));
+        System.out.println(collect);
+        System.out.println("-------FIND THE Unique NUMBER OF DEPARTMENTS --------");
+        employeeData.stream().map(Employee::getDepartment).distinct().forEach(s -> System.out.println(s));
+        System.out.println("-------FIND THE Average Age of Male and Female --------");
+        Map<String, Double> averageAge = employeeData.stream().collect(Collectors.groupingBy(Employee::getGender, Collectors.averagingDouble(Employee::getAge)));
+        System.out.println(averageAge);
+        System.out.println("-------FIND THE DETAILS OF HIGHEST PAID EMPLOYEE --------");
+        Optional<Employee> collect1 = employeeData.stream().collect(Collectors.maxBy(Comparator.comparing(Employee::getSalary)));
+        System.out.println(collect1.get());
+        System.out.println("-------FIND THE EMPLOYEE WHO JOINED OF HIGHEST PAID EMPLOYEE --------");
+        employeeData.stream().filter(employee -> employee.yearOfJoining>2015).collect(Collectors.toList()).forEach(employee -> System.out.print(employee));
+        System.out.println("-------COUNT NUMBER OF EMPLOYEE IN EACH DEPARTMENT --------");
+        Map<String, Long> collect2 = employeeData.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.counting()));
+        System.out.println(collect2);
+        System.out.println("----WHAT IS AVERAGE SALARY OF EACH DEPARTMENT----");
+        Map<String, Double> collect3 = employeeData.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.averagingDouble(Employee::getSalary)));
+        System.out.println(collect3);
+        System.out.println("----Get the details of youngest male employee in the product development department?-----");
+        Optional<Employee> collect4 = employeeData.stream().
+                filter(employee -> employee.department.equalsIgnoreCase("Product Development") &&
+                        employee.getGender().equalsIgnoreCase("male")).min(Comparator.comparingDouble(Employee::getAge));
+        System.out.println(collect4.get());
+        System.out.println("-----Who has the most working experience in the organization?--------");
+        Optional<Employee> first = employeeData.stream().sorted(Comparator.comparing(Employee::getYearOfJoining)).findFirst();
+        System.out.println(first);
+
+    }
+
+
+}
